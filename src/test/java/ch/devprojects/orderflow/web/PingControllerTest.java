2c87096611c8ea0c05caf926b27f61f11cc7e236
Giovanni Suter <giovanni.suter@me.com>
2025-11-18 10:56:51 +0100

    Updated test message for /ping endpoint

--- src/test/java/ch/devprojects/orderflow/web/PingControllerTest.java
+++ src/test/java/ch/devprojects/orderflow/web/PingControllerTest.java
@@ -30,7 +30,7 @@ class PingControllerTest {
 	@Test
 	void ping_shouldReturnExactAliveMessage() throws Exception {
 		mockMvc.perform(get("/api/ping")).andExpect(status().isOk())
-				.andExpect(content().string("OrderFlow API is alive - v2"));
+				.andExpect(content().string("OrderFlow API is alive - v2.1"));
 	}
 
 	@Test
