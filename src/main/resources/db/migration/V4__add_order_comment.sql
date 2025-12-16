3be6bf573947342df19b84000a2a53584d043893
Giovanni Suter <giovanni.suter@me.com>
2025-12-16 16:40:32 +0100

    Fix Flyway migration: restore V4, add V5 rename comment to description

---
+++ src/main/resources/db/migration/V4__add_order_comment.sql
@@ -0,0 +1,2 @@
+ALTER TABLE orders
+  ADD COLUMN comment VARCHAR(2000) NULL;
--- src/main/resources/db/migration/V4__add_order_description.sql
+++
@@ -1,5 +0,0 @@
--- Adds a persistent description field to orders.
--- This is required because the frontend sends "description" and we want it stored in DB.
-
-ALTER TABLE orders
-  ADD COLUMN description VARCHAR(2000) NULL;
